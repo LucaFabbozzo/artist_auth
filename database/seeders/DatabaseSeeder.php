2d629bd8019221c09ea2353bb5f9985d0a66fedf
Stefania De Flaviis <stefania.deflaviis@gmail.com>
2023-01-25 17:33:52 +0100

    update database seeder with seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,7 +17,9 @@ class DatabaseSeeder extends Seeder
         $this->call([
             MuseumSeeder::class,
             ArtistsTableSeeder::class,
-            ArtworksTableSeeder::class
+            ArtworksTableSeeder::class,
+            ArtistsMuseumsTableSeeder::class,
+            ArtworkMuseumSeeder::class
         ]);
     }
 }
